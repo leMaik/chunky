e44bd115375a849c5790dc7c89f3d800ea270f04
Maik Marschner <an@maik.tk>
2020-06-01 20:36:53 +0200

    Use String.join to join strings.

--- chunky/src/java/se/llbit/chunky/resources/TexturePackLoader.java
+++ chunky/src/java/se/llbit/chunky/resources/TexturePackLoader.java
@@ -3650,14 +3650,7 @@ public class TexturePackLoader {
       Log.info(message.toString());
     }
     if (remember) {
-      StringBuilder paths = new StringBuilder();
-      for (String path : texturePacks) {
-        if (paths.length() > 0) {
-          paths.append(File.pathSeparator);
-        }
-        paths.append(path);
-      }
-      PersistentSettings.setLastTexturePack(paths.toString());
+      PersistentSettings.setLastTexturePack(String.join(File.pathSeparator, texturePacks));
     }
   }
 
