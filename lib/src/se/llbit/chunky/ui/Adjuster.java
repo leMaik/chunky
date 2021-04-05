79924f8f0ff7ac6be69bf425d228a811cfef60dc
Maik Marschner <an@maik.tk>
2021-04-05 18:59:28 +0200

    Improve adjuster dragging, hide and lock the cursor while dragging.

--- lib/src/se/llbit/chunky/ui/Adjuster.java
+++ lib/src/se/llbit/chunky/ui/Adjuster.java
@@ -16,6 +16,9 @@
  */
 package se.llbit.chunky.ui;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.util.function.Consumer;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.Property;
 import javafx.beans.property.SimpleStringProperty;
@@ -30,24 +33,31 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.util.converter.NumberStringConverter;
 
-import java.util.function.Consumer;
-
 /**
  * A control for editing numeric values with a text field.
  */
 public abstract class Adjuster<T extends Number> extends HBox {
+
   private final StringProperty name = new SimpleStringProperty("Name");
   protected final Label nameLbl = new Label();
   protected final TextField valueField = new TextField();
   protected final Property<Number> value;
   private ChangeListener<Number> listener;
   private double dragStart;
+  private double dragStartScreenX;
+  private double dragStartScreenY;
+  private double firstDragStart;
+  private double delta = 0;
+  private double dragStartValue;
+  private boolean altWasPressed;
+  private boolean shiftWasPressed;
 
   protected Adjuster(Property<Number> value) {
     this.value = value;
     nameLbl.textProperty().bind(Bindings.concat(name, ":"));
     nameLbl.setOnMousePressed(this::handleDragStart);
     nameLbl.setOnMouseDragged(this::handleDragged);
+    nameLbl.setOnMouseReleased(this::handleReleased);
     nameLbl.setCursor(Cursor.H_RESIZE);
     setAlignment(Pos.CENTER_LEFT);
     setSpacing(10);
@@ -56,19 +66,59 @@ public abstract class Adjuster<T extends Number> extends HBox {
     getChildren().addAll(nameLbl, valueField);
   }
 
+  private void handleReleased(MouseEvent mouseEvent) {
+    nameLbl.setCursor(Cursor.H_RESIZE);
+    try {
+      new Robot().mouseMove((int) dragStartScreenX, (int) dragStartScreenY);
+    } catch (AWTException e) {
+      e.printStackTrace();
+    }
+  }
+
   private void handleDragStart(MouseEvent mouseEvent) {
     dragStart = mouseEvent.getX();
+    dragStartScreenX = mouseEvent.getScreenX();
+    dragStartScreenY = mouseEvent.getScreenY();
+    firstDragStart = dragStart;
+    delta = 0;
+    dragStartValue = value.getValue().doubleValue();
+    altWasPressed = mouseEvent.isAltDown();
+    shiftWasPressed = mouseEvent.isShiftDown();
+    nameLbl.setCursor(Cursor.NONE);
   }
 
   private void handleDragged(MouseEvent mouseEvent) {
-    double delta = (mouseEvent.getX() - dragStart);
-    if (mouseEvent.isShiftDown()) {
-      delta *= 5;
-    } else if (mouseEvent.isAltDown()) {
-      delta *= 0.1;
+    // for very smooth dragging (if the delta is very small) we need to save the
+    // delta for the current movement (alt+shift combination) and apply that whenever
+    // the mouse is moved
+    if (altWasPressed != mouseEvent.isAltDown() || shiftWasPressed != mouseEvent.isShiftDown()) {
+      dragStartValue = value.getValue().doubleValue();
+      this.delta = 0;
+    } else {
+      double scale = 1;
+      if (mouseEvent.isShiftDown()) {
+        scale *= 5;
+      } else if (mouseEvent.isAltDown()) {
+        scale *= 0.1;
+      }
+      this.delta += (mouseEvent.getX() - dragStart);
+      T newValue = clamp(dragStartValue + this.delta * scale);
+      this.delta -= (dragStartValue + this.delta * scale - newValue.doubleValue()) * scale;
+      setAndUpdate(newValue);
     }
-    setAndUpdate(clamp(value.getValue().doubleValue() + delta));
     dragStart = mouseEvent.getX();
+
+    if (Math.abs(mouseEvent.getScreenX() - dragStartScreenX) > 20) {
+      try {
+        new Robot().mouseMove((int) dragStartScreenX, (int) dragStartScreenY);
+        dragStart = firstDragStart;
+      } catch (AWTException e) {
+        e.printStackTrace();
+      }
+    }
+
+    altWasPressed = mouseEvent.isAltDown();
+    shiftWasPressed = mouseEvent.isShiftDown();
   }
 
   public void setName(String name) {
