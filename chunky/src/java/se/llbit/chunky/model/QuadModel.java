d6fcaccfbde705ea5858c50ddbaeaa6b4e0a3504
Maik Marschner <an@maik.tk>
2021-04-05 17:50:49 +0200

    Add support for all specular maps to the quad model.

--- chunky/src/java/se/llbit/chunky/model/QuadModel.java
+++ chunky/src/java/se/llbit/chunky/model/QuadModel.java
@@ -30,11 +30,19 @@ public abstract class QuadModel implements BlockModel {
     Texture[] textures = getTextures();
     TintType[] tintedQuads = getTintedQuads();
 
+    float[] color = null;
+    double emittanceValue = 0;
+    double reflectanceValue = 0;
+    double roughnessValue = 0;
+    float metalnessValue = 0;
+    Quad hitQuad = null;
+    Texture hitTexture = null;
+
     for (int i = 0; i < quads.length; ++i) {
       Quad quad = quads[i];
       if (quad.intersect(ray)) {
-        float[] color = textures[i].getColor(ray.u, ray.v);
-        if (color[3] > Ray.EPSILON) {
+        float[] c = textures[i].getColor(ray.u, ray.v);
+        if (c[3] > Ray.EPSILON) {
           TintType tintType = tintedQuads == null ? TintType.NONE : tintedQuads[i];
           if (tintType != TintType.NONE) {
             float[] biomeColor;
@@ -53,20 +61,39 @@ public abstract class QuadModel implements BlockModel {
                 biomeColor = new float[]{1, 1, 1};
                 break;
             }
-            color[0] *= biomeColor[0];
-            color[1] *= biomeColor[1];
-            color[2] *= biomeColor[2];
+            c[0] *= biomeColor[0];
+            c[1] *= biomeColor[1];
+            c[2] *= biomeColor[2];
           }
-          ray.color.set(color);
+          color = c;
+          hitQuad = quad;
+          hitTexture = textures[i];
+          emittanceValue = hitTexture.getEmittanceAt(ray.u, ray.v);
+          reflectanceValue = hitTexture.getReflectanceAt(ray.u, ray.v);
+          roughnessValue = hitTexture.getRoughnessAt(ray.u, ray.v);
+          metalnessValue = hitTexture.getMetalnessAt(ray.u, ray.v);
           ray.t = ray.tNext;
           ray.n.set(quad.n);
-          NormalMap.apply(ray, quad, textures[i]);
           hit = true;
         }
       }
     }
 
     if (hit) {
+      double px = ray.o.x - Math.floor(ray.o.x + ray.d.x * Ray.OFFSET) + ray.d.x * ray.tNext;
+      double py = ray.o.y - Math.floor(ray.o.y + ray.d.y * Ray.OFFSET) + ray.d.y * ray.tNext;
+      double pz = ray.o.z - Math.floor(ray.o.z + ray.d.z * Ray.OFFSET) + ray.d.z * ray.tNext;
+      if (px < 0 || px > 1 || py < 0 || py > 1 || pz < 0 || pz > 1) {
+        // TODO this check is only really needed for wall torches
+        return false;
+      }
+
+      NormalMap.apply(ray, hitQuad, hitTexture);
+      ray.color.set(color);
+      ray.emittanceValue = emittanceValue;
+      ray.reflectanceValue = reflectanceValue;
+      ray.roughnessValue = roughnessValue;
+      ray.metalnessValue = metalnessValue;
       ray.distance += ray.t;
       ray.o.scaleAdd(ray.t, ray.d);
     }
