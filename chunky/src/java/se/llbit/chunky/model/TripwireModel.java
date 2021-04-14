c5df3134d7a8d2344d516bdee8b02d418b3e0780
Alex <alexhliu02@gmail.com>
2021-04-14 00:52:12 -0700

    Blocks...

---
+++ chunky/src/java/se/llbit/chunky/model/TripwireModel.java
@@ -0,0 +1,353 @@
+package se.llbit.chunky.model;
+
+import se.llbit.chunky.resources.Texture;
+import se.llbit.math.Quad;
+import se.llbit.math.Vector3;
+import se.llbit.math.Vector4;
+
+import java.util.Arrays;
+
+import static se.llbit.chunky.world.BlockData.*;
+import static se.llbit.chunky.world.BlockData.CONNECTED_SOUTH;
+
+public class TripwireModel extends QuadModel {
+  //region tripwireN
+  private static final Quad[] tripwireN = {
+      // cube1
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+      // cube2
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+      // cube3
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 12 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 12 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 12 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+  };
+  //endregion
+
+  //region tripwireNE
+  private static final Quad[] tripwireNE = {
+      // cube1
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+      // cube2
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+      // cube3
+      new Quad(
+          new Vector3(8 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector3(12 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector3(8 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(8 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector3(12 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector3(8 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+      // cube4
+      new Quad(
+          new Vector3(12 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector3(16 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector3(12 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(12 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector3(16 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector3(12 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+  };
+  //endregion
+
+  //region tripwireNS
+  private static final Quad[] tripwireNS = {
+      // cube1
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+      // cube2
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+      // cube3
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 12 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 12 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 12 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+      // cube4
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 12 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 16 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 12 / 16.0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 16 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 12 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 16 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+  };
+  //endregion
+
+  //region tripwireNSE
+  private static final Quad[] tripwireNSE = {
+      // cube1
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+      // cube2
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+      // cube3
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 12 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 12 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 12 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+      // cube4
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 12 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 16 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 12 / 16.0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 16 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 12 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 16 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+      // cube5
+      new Quad(
+          new Vector3(8 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector3(12 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector3(8 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(8 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector3(12 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector3(8 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+      // cube6
+      new Quad(
+          new Vector3(12 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector3(16 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector3(12 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(12 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector3(16 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector3(12 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+  };
+  //endregion
+
+  //region tripwireNSEW
+  private static final Quad[] tripwire_NSEW = {
+      // cube1
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+      // cube2
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 4 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+      // cube3
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 12 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 12 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 8 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 12 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+      // cube4
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 12 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 16 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 12 / 16.0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 16 / 16.0),
+          new Vector3(7.75 / 16.0, 1.5 / 16.0, 12 / 16.0),
+          new Vector3(8.25 / 16.0, 1.5 / 16.0, 16 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+      // cube5
+      new Quad(
+          new Vector3(0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector3(4 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector3(0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector3(4 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector3(0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+      // cube6
+      new Quad(
+          new Vector3(4 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector3(8 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector3(4 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(4 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector3(8 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector3(4 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+      // cube7
+      new Quad(
+          new Vector3(8 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector3(12 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector3(8 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(8 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector3(12 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector3(8 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+      // cube8
+      new Quad(
+          new Vector3(12 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector3(16 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector3(12 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector4(0, 16 / 16.0, 14 / 16.0, 16 / 16.0)),
+      new Quad(
+          new Vector3(12 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector3(16 / 16.0, 1.5 / 16.0, 7.75 / 16.0),
+          new Vector3(12 / 16.0, 1.5 / 16.0, 8.25 / 16.0),
+          new Vector4(0, 16 / 16.0, 16 / 16.0, 14 / 16.0)),
+  };
+  //endregion
+
+  private static final Quad[][] model = new Quad[16][];
+
+  static {
+    model[0] = tripwireNS; // If no side is elected, render north-south.
+    model[CONNECTED_NORTH | CONNECTED_SOUTH | CONNECTED_EAST | CONNECTED_WEST] = tripwire_NSEW;
+    model[CONNECTED_NORTH | CONNECTED_EAST | CONNECTED_SOUTH] = tripwireNSE;
+    model[CONNECTED_EAST | CONNECTED_SOUTH | CONNECTED_WEST] = Model.rotateY(model[CONNECTED_NORTH | CONNECTED_EAST | CONNECTED_SOUTH]);
+    model[CONNECTED_SOUTH | CONNECTED_WEST | CONNECTED_NORTH] = Model.rotateY(model[CONNECTED_EAST | CONNECTED_SOUTH | CONNECTED_WEST]);
+    model[CONNECTED_WEST | CONNECTED_NORTH | CONNECTED_EAST] = Model.rotateY(model[CONNECTED_SOUTH | CONNECTED_WEST | CONNECTED_NORTH]);
+    model[CONNECTED_NORTH | CONNECTED_SOUTH] = tripwireNS;
+    model[CONNECTED_EAST | CONNECTED_WEST] = Model.rotateY(model[CONNECTED_NORTH | CONNECTED_SOUTH]);
+    model[CONNECTED_NORTH | CONNECTED_EAST] = tripwireNE;
+    model[CONNECTED_EAST | CONNECTED_SOUTH] = Model.rotateY(model[CONNECTED_NORTH | CONNECTED_EAST]);
+    model[CONNECTED_SOUTH | CONNECTED_WEST] = Model.rotateY(model[CONNECTED_EAST | CONNECTED_SOUTH]);
+    model[CONNECTED_WEST | CONNECTED_NORTH] = Model.rotateY(model[CONNECTED_SOUTH | CONNECTED_WEST]);
+    model[CONNECTED_NORTH] = tripwireN;
+    model[CONNECTED_EAST] = Model.rotateY(model[CONNECTED_NORTH]);
+    model[CONNECTED_SOUTH] = Model.rotateY(model[CONNECTED_EAST]);
+    model[CONNECTED_WEST] = Model.rotateY(model[CONNECTED_SOUTH]);
+  }
+
+  private final Quad[] quads;
+  private final Texture[] textures;
+
+  public TripwireModel(int connections) {
+    quads = model[connections];
+    textures = new Texture[quads.length];
+    Arrays.fill(textures, Texture.tripwire);
+  }
+
+  @Override
+  public Quad[] getQuads() {
+    return quads;
+  }
+
+  @Override
+  public Texture[] getTextures() {
+    return textures;
+  }
+}
