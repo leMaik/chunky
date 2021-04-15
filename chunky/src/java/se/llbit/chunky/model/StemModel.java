86690e7a9129058c140ed8c2a347768baafbad88
Alex <alexhliu02@gmail.com>
2021-04-14 22:59:19 -0700

    Stems and Logs.

--- chunky/src/java/se/llbit/chunky/model/StemModel.java
+++ chunky/src/java/se/llbit/chunky/model/StemModel.java
@@ -7,37 +7,29 @@ import se.llbit.math.Vector3;
 import se.llbit.math.Vector4;
 
 public class StemModel extends QuadModel {
-  private static final Quad[] growth = new Quad[2];
-  private static final Quad[] ripe = {
-      new DoubleSidedQuad(new Vector3(0, 0, .5), new Vector3(1, 0, .5), new Vector3(0, 1, .5),
-          new Vector4(0, 1, 0, 1)),
-      new DoubleSidedQuad(new Vector3(0, 0, .5), new Vector3(1, 0, .5),
-          new Vector3(0, 1, .5), new Vector4(1, 0, 0, 1)),
-      new DoubleSidedQuad(new Vector3(.5, 0, 0), new Vector3(.5, 0, 1),
-          new Vector3(.5, 1, 0), new Vector4(0, 1, 0, 1)),
-      new DoubleSidedQuad(new Vector3(.5, 0, 0), new Vector3(.5, 0, 1),
-          new Vector3(.5, 1, 0), new Vector4(1, 0, 0, 1)),
-  };
-
-  static {
-    int height = 3;
-    growth[0] = new DoubleSidedQuad(new Vector3(0, 0, 0), new Vector3(1, 0, 1),
-        new Vector3(0, (height + 1) / 8., 0), new Vector4(0, 1, (7 - height) / 8., 1));
-    growth[1] = new DoubleSidedQuad(new Vector3(1, 0, 0), new Vector3(0, 0, 1),
-        new Vector3(1, (height + 1) / 8., 0), new Vector4(0, 1, (7 - height) / 8., 1));
-  }
-
-  private static final Texture[] textures = {
-      Texture.stemStraight, Texture.stemStraight, Texture.stemBent
+  private static final Texture[] textures = {Texture.stemStraight, Texture.stemStraight};
+  private static final Tint[][] stemColors = {
+      {new Tint(0xFF00E210), new Tint(0xFF00E210)},
+      {new Tint(0xFF00E210), new Tint(0xFF00E210)},
+      {new Tint(0xFF00E210), new Tint(0xFF00E210)},
+      {new Tint(0xFF00CC06), new Tint(0xFF00CC06)},
+      {new Tint(0xFF5FC803), new Tint(0xFF5FC803)},
+      {new Tint(0xFF65C206), new Tint(0xFF65C206)},
+      {new Tint(0xFFA0B800), new Tint(0xFFA0B800)},
+      {new Tint(0xFFBFB600), new Tint(0xFFBFB600)},
   };
 
   private final Quad[] quads;
+  private final Tint[] tints;
 
-  public StemModel(int facing) {
-    quads = new Quad[3];
-    quads[0] = growth[0];
-    quads[1] = growth[1];
-    quads[2] = ripe[facing];
+  public StemModel(int height) {
+    this.quads = new Quad[] {
+        new DoubleSidedQuad(new Vector3(0, 0, 0), new Vector3(1, 0, 1),
+            new Vector3(0, (height + 1) / 8., 0), new Vector4(0, 1, (7 - height) / 8., 1)),
+        new DoubleSidedQuad(new Vector3(1, 0, 0), new Vector3(0, 0, 1),
+            new Vector3(1, (height + 1) / 8., 0), new Vector4(0, 1, (7 - height) / 8., 1)),
+    };
+    this.tints = stemColors[height];
   }
 
   @Override
@@ -48,5 +40,10 @@ public class StemModel extends QuadModel {
   @Override
   public Texture[] getTextures() {
     return textures;
+  }
+
+  @Override
+  public Tint[] getTints() {
+    return tints;
   }
 }
