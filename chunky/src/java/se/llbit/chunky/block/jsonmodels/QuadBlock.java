7566cbcdeeeee5e9544c57249913ce5ac584b47c
Maik Marschner <an@maik.tk>
2022-07-03 22:22:26 +0200

    Fix rendering of custom block models that are bigger than one block.

--- chunky/src/java/se/llbit/chunky/block/jsonmodels/QuadBlock.java
+++ chunky/src/java/se/llbit/chunky/block/jsonmodels/QuadBlock.java
@@ -55,7 +55,8 @@ public class QuadBlock extends Block implements QuadModel, ModelBlock {
   }
 
   @Override
-  public boolean isBlockWithEntity() {
+  public boolean isBlockEntity() {
+    // TODO we could further optimize this by only putting the out-of-bounds quads into the BVH and rendering the rest (if any) as block
     return isEntity;
   }
 
